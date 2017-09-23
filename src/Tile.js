dcd360ff86255c7b95c79ed5228270f59b9eee9a
Kushal <kushal.prabhakar@in.tesco.com>
2017-09-23 13:58:23 +0530

    game to find pokemond algorithm done

--- src/Tile.js
+++ src/Tile.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Tile extends React.Component {
+
+  constructor() {
+    super();
+    this.state={
+      className: 'question'
+    }
+    this.updateTileStatus = this.updateTileStatus.bind(this);
+  }
+
+  updateTileStatus(event) {
+    event.stopPropagation();
+    if(event.currentTarget.className === 'question') {
+      const getclassName = this.props.setTileStatus(event.currentTarget.id);
+      this.setState({className: getclassName});
+    }
+  }
   render() {
     return (
-      <td>Tile</td>
+      <td className={ this.state.className } onClick={ this.updateTileStatus } id={ this.props.id }>
+      </td>
     )
   }
+}
+
+Tile.propTypes= {
+  id: PropTypes.number.isRequired
 }
 
 export default Tile;
--- src/TileBoard.js
+++ src/TileBoard.js
@@ -5,11 +5,38 @@ import Tile from './Tile';
 class TileBoard extends React.Component {
   constructor() {
     super();
-    this.diamondPositions = [1,2,3,4,5,6,7,8];
+    this.diamondPositions = [2,5,7,32,54,63,21,44];
+    this.setTileStatus = this.setTileStatus.bind(this);
+    this.checkWhichArrow = this.checkWhichArrow.bind(this);
   }
-
+  setTileStatus(id) {
+    return this.diamondPositions.includes(parseInt(id)) ? ('pokeball') : this.checkWhichArrow(id);
+  }
+  checkWhichArrow(id) {
+    let x1 = Math.ceil(id/8);
+    let y1 = id%8;
+    let leastXValue;
+    let leastYValue;
+    let x2;
+    let y2;
+    let maxDistance = 12;
+    for(let i=0;i<this.diamondPositions.length;i++){
+      x2 = Math.ceil(this.diamondPositions[i]/8)
+      y2 = this.diamondPositions[i]%8;
+      let temp = (Math.sqrt(Math.pow(Math.abs(x1-x2),2)+Math.pow(Math.abs(y1-y2),2)));
+      if(temp<maxDistance) {
+        maxDistance = temp;
+        leastXValue = x2;
+        leastYValue = y2;
+      }
+    }
+    if(Math.abs(leastXValue-x1)<=Math.abs(leastYValue-y1)){
+      return (leastYValue-y1) > 0 ? 'rightArrow' : 'leftArrow';
+    }
+    return (leastXValue-x1) > 0 ? 'downArrow': 'upArrow';
+  }
   render() {
-    const tilesColumns = [...Array(64).keys()].map((e) => (<Tile/>));
+    const tilesColumns = [...Array(64).keys()].map((e) => (<Tile id={ e } setTileStatus={ this.setTileStatus }/>));
     const tilesRows=[];
     for(var i=0;i<8;i++){
       var rowElement = tilesColumns.slice(i*8, (i+1)*8);
